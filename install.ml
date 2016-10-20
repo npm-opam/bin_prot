77a47334571a3ba674ae682b46ee5419f9c7c380
Yunxing Dai <yunxing@fb.com>
2016-10-20 14:29:58 -0700

    init commit

---
+++ install.ml
@@ -0,0 +1,10 @@
+#use "topfind";;
+#require "js-build-tools.oasis2opam_install";;
+
+open Oasis2opam_install;;
+
+generate ~package:"bin_prot"
+  [ oasis_lib "bin_prot"
+  ; file "META" ~section:"lib"
+  ; oasis_lib "bin_prot_xen"
+  ]
